d4f1f598c2e49f6275aa87b85e75090b0fb9a1d0
antonis-angelis <agelisadonis@gmail.com>
2023-07-16 20:08:33 +0300

    custom getUser decorator and http ok status for sign in

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, UseGuards, Get, Req } from '@nestjs/common';
+import { Controller, UseGuards, Get } from '@nestjs/common';
 import { JwtGuard } from 'src/auth/guard/jwt.guard';
-import { Request } from 'express';
+import { GetUser } from 'src/auth/decorator';
+import { User } from '@prisma/client';
 
+@UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
-  @UseGuards(JwtGuard)
   @Get('me')
-  getMe(@Req() req: Request) {
-    return req.user;
+  getMe(@GetUser() user: User) {
+    return user;
   }
 }
